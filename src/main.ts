be52f559d567e0a9047f02decfc23ed2df325fc1
Leonardo Geranio <leonardo.geranio@gmail.com>
2026-02-01 18:44:53 -0300

    enable CORS

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,20 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalGuards(new AppKeyGuard());
 
+  app.enableCors({
+    origin: '*', // em dev pode deixar *, depois você restringe se quiser
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: [
+      'Content-Type',
+      'Accept',
+      'x-app-key',
+      'x-appkey',
+      'x-app_key',
+      'x-signature',
+      'x-timestamp',
+    ],
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
